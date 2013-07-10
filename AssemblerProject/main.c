bfbd81e36e9247d37171f40ed4143c3d7d7e3955
kylelol <kyle.kirkland0528@me.com>
2013-07-10 11:51:43 -0400

    Initial Commit

---
+++ AssemblerProject/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  AssemblerProject
+//
+//  Created by Kyle Kirkland on 7/10/13.
+//  Copyright (c) 2013 Kyle Kirkland. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
