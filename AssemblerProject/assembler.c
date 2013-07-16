6b295105902f1c84cbbadcf6cf36a6b9209fb02a
Kylelol <kyle.kirland0528@me.com>
2013-07-16 19:15:42 -0400

    Added in register Labels table.

--- AssemblerProject/assembler.c
+++ AssemblerProject/assembler.c
@@ -21,60 +21,69 @@
 #define MIPS_REGISTER_ARRAY_SIZE 32
 #define ZERO_CHAR_VALUE 48
 
-const char *mipsAssemblerDirectiveInstructions[3] = { ".ent", // 0
-                                                  ".txt", // 1
-                                                  ".end" // 2
-                                                };
-
-const char *mipsAssemblerInstructions[15] = { "add",
-                                          "div",
-                                          "jr",
-                                          "mflo",
-                                          "mult",
-                                          "sll",
-                                          "sub",
-                                          "addi",
-                                          "bltz",
-                                          "bne",
-                                          "lw",
-                                          "sw",
-                                          "j",
-                                          "jal"
-                                        };
-
-const char *mipsRegisters[32] = { "$zero",
-    "$at",
-    "$v0",
-    "$v1",
-    "$a0",
-    "$a1",
-    "$a2",
-    "$a3",
-    "$t0",
-    "$t1",
-    "$t2",
-    "$t3",
-    "$t4",
-    "$t5",
-    "$t6",
-    "$t7",
-    "$s0",
-    "$s1",
-    "$s2",
-    "$s3",
-    "$s4",
-    "$s5",
-    "$s6",
-    "$s7",
-    "$t8",
-    "$t9",
-    "$k0",
-    "$k1",
-    "$gp",
-    "$sp",
-    "$fp",
-    "$ra"
+const char *mipsAssemblerDirectiveInstructions[3] =
+{
+    ".ent", // 0
+    ".txt", // 1
+    ".end" // 2
+    
 };
+
+const char *mipsAssemblerInstructions[15] =
+{
+    "add",
+    "div",
+    "jr",
+    "mflo",
+    "mult",
+    "sll",
+    "sub",
+    "addi",
+    "bltz",
+    "bne",
+    "lw",
+    "sw",
+    "j",
+    "jal"
+};
+
+const char *mipsRegisters[32] =
+{
+    "zero",
+    "at",
+    "v0",
+    "v1",
+    "a0",
+    "a1",
+    "a2",
+    "a3",
+    "t0",
+    "t1",
+    "t2",
+    "t3",
+    "t4",
+    "t5",
+    "t6",
+    "t7",
+    "s0",
+    "s1",
+    "s2",
+    "s3",
+    "s4",
+    "s5",
+    "s6",
+    "s7",
+    "t8",
+    "t9",
+    "k0",
+    "k1",
+    "gp",
+    "sp",
+    "fp",
+    "ra"
+};
+
+label_node_t **labelArray;
 
 void
 assemble(
@@ -82,17 +91,36 @@ assemble(
          vector_uint_t * machine_code
         )
 {
+    // Allocate the label array.
+    labelArray = (label_node_t**)malloc(sizeof(label_node_t*) * (strlen(mips_assembly->elements[0]) / 2) );
+    
     // 2D array to hold the instructions. 
     char **instructions = (char**)malloc(sizeof(char*) * strlen(mips_assembly->elements[0]));
     char *newMipsInstruction = strtok(mips_assembly->elements[0], "\r");
        
     // Parse the instructions from the vector_string_t into the 2D array.
     int i=0;
+    int labelIndex=0;
     while (newMipsInstruction != NULL)
     {
+        // Allocate the space.
         instructions[i] = (char*)malloc(sizeof(char) * strlen(newMipsInstruction));
         
+        // Store the instruction.
         instructions[i] = newMipsInstruction;
+        
+        // If a label, store the index, and label string for lookup later.
+        if (instructionIsALabel(newMipsInstruction))
+        {
+            label_node_t *node = (label_node_t*)malloc(sizeof(label_node_t));
+            node->index = i;
+            node->label = newMipsInstruction;
+            labelArray[labelIndex] = node;
+                        
+            labelIndex++;
+        }
+        
+        // Move to the next instruction.
         newMipsInstruction = strtok(NULL, "\r");
         
         printf("%s\n", instructions[i]);
@@ -146,7 +174,7 @@ assemble(
         // Otherwise check which MIPS instruction is being executed. 
         else
         {
-            // Get the instruction Opcode.
+            // Get the instruction.
             char *instruction = instructions[i];
             instruction = strtok(instruction, " ");
             
@@ -157,6 +185,7 @@ assemble(
                 case add:
                 {
                     machine_code->elements[i] = mipsInstructionAdd( instruction );
+                    printf("%d\n", machine_code->elements[i]);
                     break;
                 }
                 case divi:
@@ -192,6 +221,7 @@ assemble(
                 case addi:
                 {
                     machine_code->elements[i] = mipsInstructionAddi(instruction);
+                    printf("%d\n",machine_code->elements[i] );
                     break;
                 }
                 case bltz:
@@ -245,7 +275,7 @@ assemble(
 
 int mipsInstructionValue( char *instruction)
 {
-    int i=0;
+    int i = 0;
     for (i = 0; i < MIPS_ASSEMBLER_INSTRUCTIONS_ARRAY_SIZE; i++)
     {
         char *tempInstruction = (char * )mipsAssemblerInstructions[i];
@@ -293,8 +323,9 @@ int convertRegisterNameToValue( char *regName )
     for (i = 0; i < MIPS_REGISTER_ARRAY_SIZE; i++)
     {
         char *tempRegisterName = (char * )mipsRegisters[i];
-
-        if (strcmp(regName, tempRegisterName) == 0)
+        int val = (int)regName[0] - ZERO_CHAR_VALUE;
+        
+        if (strcmp(regName, tempRegisterName) == 0 || val == i)
             return i;
     }
     
@@ -308,7 +339,7 @@ unsigned int mipsInstructionAdd( char *instruction)
     unsigned int returnValue = 0;
     
     // Get the value of R[rd].
-    instruction = strtok(NULL, " ,");
+    instruction = strtok(NULL, " $,");
     unsigned int registerD = convertRegisterNameToValue(instruction);
     
     //Get the value of R[rs].
@@ -320,7 +351,7 @@ unsigned int mipsInstructionAdd( char *instruction)
     unsigned int registerT = (int)instruction[0] - ZERO_CHAR_VALUE;
     
     // Generate the machine code.
-    returnValue = (registerS << 21) + (registerT << 16) + (10 << 11) + 32;
+    returnValue = (registerS << 21) + (registerT << 16) + (registerD << 11) + 32;
         
     return returnValue;
     
@@ -333,7 +364,7 @@ unsigned int mipsInstructionAddi( char *instruction)
     
     // Get the value of R[rt]
     instruction = strtok(NULL, " $,");
-    unsigned int registerT = (int)instruction[0] - ZERO_CHAR_VALUE;
+    unsigned int registerT = convertRegisterNameToValue(instruction);
     
     // Get the value of R[rs]
     instruction = strtok(NULL, " $,");
--- AssemblerProject/assembler.h
+++ AssemblerProject/assembler.h
@@ -31,6 +31,13 @@ assemble(
 /*******************************************************************************
  ***** DEFINE YOUR FUNCTIONS BELOW *********************************************
  ******************************************************************************/
+typedef
+struct label_node
+{
+    int index;
+    char * label;
+}
+label_node_t;
 
 typedef enum { ent, txt, end} mipsAssemblerDirectiveEnum;
 typedef enum {add, divi, jr, mflo, mult, sll, sub, addi, bltz, bne, lw, sw, j, jal} mipsInstructionEnum;
